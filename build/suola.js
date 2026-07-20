2bde002b8ce4a757c1329a9e299401bc88c57cdf
Teemu Autto <teemu@rautakuu.org>
2026-07-20 12:27:23 +0000

    Move suola.js into paatti.

--- build/suola.js
+++
@@ -1,9 +0,0 @@
-async function initSuola(suolaUrl) {
-    // Fallback for non-extension runs.
-    if (!suolaUrl) {
-        suolaUrl = "js.wasm";
-    }
-    const go = new Go();
-    const result = await WebAssembly.instantiateStreaming(fetch(suolaUrl), go.importObject);
-    go.run(result.instance);
-}
